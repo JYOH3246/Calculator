c85b62dd53a4de826fd7b2975d8e8550edf9969d
재영 <jeayeong70@gmail.com>
2023-12-05 20:56:58 +0900

    lv3 branch Calculator.kt

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,9 +1,9 @@
-open class Calculator {
+open class Calculator(_num1:Float, _num2:Float) {
     var num1 :Float
     var num2 :Float
 
 
-    constructor(_num1:Float, _num2:Float) {
+    init {
         num1 = _num1
         num2 = _num2
 
