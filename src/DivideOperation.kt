6ea9c0a2ac1dd01569981091b27b0b0158e5590c
재영 <jeayeong70@gmail.com>
2023-12-06 11:59:18 +0900

    lv 4 나눗셈 : input값이 정수일 때 .0 붙는 부분을 수정

--- src/DivideOperation.kt
+++ src/DivideOperation.kt
@@ -1,9 +1,45 @@
-class DivideOperation (override var num1:Double,override var num2:Double) : AbstractOperation() {
+import kotlin.math.roundToInt
+
+class DivideOperation (override var num1:Double, override var num2:Double) : AbstractOperation() {
     var resultDiv : Double = num1/num2
     init {
         this.resultDiv = resultDiv
     }
     override fun operatingCalc (num1:Double,num2: Double) {
-        println("${num1} / ${num2} = ${resultDiv}")
+        if (num1*10%10== 0.toDouble()&&num2*10%10== 0.toDouble()){
+            if (resultDiv*10%10==0.toDouble()) {
+                println("${num1.roundToInt()} / ${num2.roundToInt()} = ${resultDiv.roundToInt()}")
+            }
+            else {
+                println("${num1.roundToInt()} / ${num2.roundToInt()} = ${resultDiv}")
+            }
+
+        }
+        else if (num1*10%10== 0.toDouble()){
+            if (resultDiv*10%10==0.toDouble()) {
+                println("${num1.roundToInt()} / ${num2} = ${resultDiv.roundToInt()}")
+            }
+            else {
+                println("${num1.roundToInt()} / ${num2} = ${resultDiv}")
+            }
+        }
+        else if (num2*10%10== 0.toDouble()){
+            if (resultDiv*10%10==0.toDouble()) {
+                println("${num1} / ${num2.roundToInt()} = ${resultDiv.roundToInt()}")
+            }
+            else {
+                println("${num1} / ${num2.roundToInt()} = ${resultDiv}")
+            }
+        }
+        else {
+            if (resultDiv*10%10==0.toDouble()) {
+                println("${num1} / ${num2} = ${resultDiv.roundToInt()}")
+            }
+            else {
+                println("${num1} / ${num2} = ${resultDiv}")
+            }
+
+        }
+
     }
 }
