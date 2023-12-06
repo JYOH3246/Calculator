56bc5f765f62b4fd6fe806fa46ffe420526ba1d6
재영 <jeayeong70@gmail.com>
2023-12-06 11:41:08 +0900

    lv 4 곱셈 : input값이 정수일 때 .0 붙는 부분을 수정

--- src/MultiplyOperation.kt
+++ src/MultiplyOperation.kt
@@ -1,9 +1,26 @@
-class MultiplyOperation (override var num1:Double,override var num2:Double) : AbstractOperation(){
+import kotlin.math.roundToInt
+
+class MultiplyOperation (override var num1:Double, override var num2:Double) : AbstractOperation(){
     var resultMulti : Double = num1*num2
     init {
         this.resultMulti = resultMulti
     }
     override fun operatingCalc (num1:Double,num2: Double) {
-        println("${num1} * ${num2} = ${resultMulti}")
+        if (num1*10%10== 0.toDouble()&&num2*10%10== 0.toDouble()){
+            println("${num1.roundToInt()} * ${num2.roundToInt()} = ${resultMulti.roundToInt()}")
+        }
+        else if (num1*10%10== 0.toDouble()){
+            println("${num1.roundToInt()} * ${num2} = ${resultMulti}")
+        }
+        else if (num2*10%10== 0.toDouble()){
+            println("${num1} * ${num2.roundToInt()} = ${resultMulti}")
+        }
+        else if (resultMulti*10%10==0.toDouble()) {
+            println("${num1} * ${num2} = ${resultMulti.roundToInt()}")
+        }
+        else {
+            println("${num1} * ${num2} = ${resultMulti}")
+        }
+
     }
 }
