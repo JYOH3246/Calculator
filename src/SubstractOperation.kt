694db3fbf929d756c37335598b14e2c4aed62ccb
재영 <jeayeong70@gmail.com>
2023-12-06 11:38:27 +0900

    lv 4 뺄셈 : input값이 정수일 때 .0 붙는 부분을 수정

--- src/SubstractOperation.kt
+++ src/SubstractOperation.kt
@@ -1,9 +1,26 @@
-class SubstractOperation(override var num1:Double,override var num2:Double) : AbstractOperation() {
+import kotlin.math.roundToInt
+
+class SubstractOperation(override var num1:Double, override var num2:Double) : AbstractOperation() {
     var resultSub : Double = num1-num2
     init {
         this.resultSub = resultSub
     }
     override fun operatingCalc (num1:Double,num2: Double) {
-        println("${num1} - ${num2} = ${resultSub}")
+        if (num1*10%10== 0.toDouble()&&num2*10%10== 0.toDouble()){
+            println("${num1.roundToInt()} - ${num2.roundToInt()} = ${resultSub.roundToInt()}")
+        }
+        else if (num1*10%10== 0.toDouble()){
+            println("${num1.roundToInt()} - ${num2} = ${resultSub}")
+        }
+        else if (num2*10%10== 0.toDouble()){
+            println("${num1} - ${num2.roundToInt()} = ${resultSub}")
+        }
+        else if (resultSub*10%10==0.toDouble()) {
+            println("${num1} - ${num2} = ${resultSub.roundToInt()}")
+        }
+        else {
+            println("${num1} - ${num2} = ${resultSub}")
+        }
+
     }
 }
